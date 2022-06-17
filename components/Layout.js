1aee5acfef1933ad663a2eb145c9f3586e45bddb
Ankit Singh <ankit.singh.zm@gmail.com>
2022-06-17 17:45:16 +0530

    improved layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,21 +1,16 @@
 import React from 'react';
-import Footer from './Footer';
 import Navbar from './Navbar';
 
 const Layout = ({ children }) => {
     return (
         <div className="relative h-screen flex flex-col justify-center items-center bg-gradient-to-r from-green-400 via-blue-400 to-violet-400">
-            <div className='fixed top-4'>
+            <div className='top-2 mb-6 sm:mb-4'>
                 <Navbar />
             </div>
 
-            <div className="container h-5/6 p-10 sm:p-16 items-center overflow-auto 
-                bg-white shadow-lg sm:rounded-3xl bg-clip-padding bg-opacity-40">
+            <div className="container h-5/6 p-10 sm:p-16 overflow-auto 
+                bg-white sm:rounded-t-3xl bg-clip-padding bg-opacity-40 flex flex-wrap">
                 {children}
-            </div>
-
-            <div className="fixed pin z-1 overflow-auto bg-smoke-light">
-                <Footer />
             </div>
         </div>
     );

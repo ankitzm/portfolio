d622899ff04a7fa8cc42894849ec03d25a9d8643
Ankit Singh <ankit.singh.zm@gmail.com>
2022-06-18 13:57:21 +0530

    added umami analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head';
 import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <script async defer data-website-id="67617051-3806-4b5f-993f-57d03147c9ef" src="https://umami-production-6c51.up.railway.app/umami.js"></script>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 

d005e79ef64757aa6ce380ece2e01e82fee075d4
Ankit Singh <ankit.singh.zm@gmail.com>
2022-06-18 13:49:49 +0530

    all icons in one place

---
+++ public/icon/index.js
@@ -0,0 +1,7 @@
+import github from "./github.png"
+import medium from "./medium.png"
+import linkedin from "./linkedin.png"
+import twitter from "./twitter.png"
+import linkIcon from "./link.png"
+
+export { github, medium, linkedin, twitter, linkIcon };

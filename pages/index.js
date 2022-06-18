189a8ece4ec7d330c3a2708d091a55576415d336
Ankit Singh <ankit.singh.zm@gmail.com>
2022-06-18 13:50:03 +0530

    Homepage updated

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,42 @@
 import Image from 'next/image'
 import profilepic from "./../public/ankitzm.png"
 import Data from "./../data/data.json"
+import { github, medium, linkedin, twitter } from "./../public/icon"
 
 export default function Home() {
   const Name = Data.name;
   const About = Data.about;
 
   return (
-    <div className='flex flex-col-reverse sm:flex-row items-center content-center'>
-      <div className="flex flex-col w-full lg:w-1/2">
-        <h1 className="text-xl tracking-wide text-gray-800 lg:text-4xl">{Name}</h1>
-        <p className="mt-4 text-gray-600">{About}</p>
-        <div className="mt-6">
-          <a href="#"
-            className="inline-block px-3 py-2 font-semibold text-center text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400">
-            Read More</a>
+    <div className='flex flex-col-reverse sm:flex-row  items-center content-center'>
+      <div className="flex flex-col w-full lg:w-1/2 py-8">
+        <h1 className="text-4xl sm:text-3xl md:text-6xl tracking-wide text-gray-800" style={{ "fontFamily": 'righteous' }}>
+          {Name}
+        </h1>
+        <p className="mt-4 text-gray-600 text-lg lg:text-xl">
+          {About}
+        </p>
+        <div className="mt-6 text-lg lg:text-xl">
+          Stalk me ↴
+          <div className='flex gap-4 ml-4 mt-2 opacity-85'>
+            <a href="https://twitter.com/ankitzm" target="_blank" rel='noreferrer'>
+              <Image src={twitter} alt="" width={30} height={30} />
+            </a>
+            <a href="https://medium.com/@ankitzm" target="_blank" rel='noreferrer'>
+              <Image src={medium} alt="" width={30} height={30} />
+            </a>
+            <a href="https://www.linkedin.com/in/ankitzm/" target="_blank" rel='noreferrer'>
+              <Image src={linkedin} alt="" width={30} height={30} />
+            </a>
+            <a href="https://github.com/ankitzm" target="_blank" rel='noreferrer'>
+              <Image src={github} alt="" width={30} height={30} />
+            </a>
+          </div>
         </div>
       </div>
       <div className="flex items-center justify-center w-full mt-2 lg:h-96 lg:w-1/2 box-content">
         <Image
-          className="object-cover w-full max-w-2xl rounded-full lg:h-full divide-x-8"
+          className="w-full max-w-2xl rounded-full lg:h-full"
           src={profilepic}
           alt="profile pic" />
       </div>

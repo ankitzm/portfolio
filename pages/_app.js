153b4dc88e5d83671a0beb6b30bb25753b268267
Ankit Singh <ankit.singh.zm@gmail.com>
2023-02-11 13:08:18 +0530

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   )
 }

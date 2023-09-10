b6db1e027cfbcafd22561266e0d6f5f2ccd77ddc
Ankit Singh <ankit.singh.zm@gmail.com>
2023-09-10 23:03:34 +0530

    added icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,8 @@
 import { Link, useLocation } from "react-router-dom"
+import { HiHome } from "react-icons/hi"
+import { GrTools } from "react-icons/gr"
+import { PiToolboxDuotone } from "react-icons/pi"
+
 
 function Navbar() {
 
@@ -6,14 +10,17 @@ function Navbar() {
         {
             'name': "Home",
             "page": "/",
+            "icon": HiHome
         },
         {
             'name': "Projects",
             "page": "/projects",
+            "icon": GrTools
         },
         {
             'name': "Expereince",
             "page": "/experience",
+            "icon": PiToolboxDuotone
         }
     ]
 
@@ -28,8 +35,8 @@ function Navbar() {
             {
                 navButtons.map(e =>
                     <Link to={e.page.toLowerCase()}>
-                        <li key={e.name} className={`bg-indigo-400 py-2 px-4 m-2 rounded-md w-fit ${currentPath == e.page ? activeClass : ""}`}>
-                            {e.name}
+                        <li key={e.name} className={`flex items-center gap-2 bg-indigo-400 py-2 px-4 m-2 rounded-md w-fit text-lg ${currentPath == e.page ? activeClass : ""} `}>
+                            {<e.icon />} {e.name.toLowerCase()}
                         </li>
                     </Link>
                 )

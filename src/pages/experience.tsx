f9347cfc1f7b42399d89147214d02ccb1e86154c
Ankit Singh <ankit.singh.zm@gmail.com>
2023-12-27 13:22:09 +0530

    Added smooth scroll !!

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,9 +1,23 @@
 import { useRef } from "react"
 import { motion, useScroll } from "framer-motion"
+import Lenis from "@studio-freight/lenis";
 import ExperienceCard from "../components/ExperienceCard";
 import experienceData from "./../data/experience.json"
 
 function Experience() {
+  const lenis = new Lenis()
+
+  // smooth scroll
+  lenis.on('scroll', (e: any) => {
+    console.log(e)
+  })
+
+  function raf(time: any) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+  requestAnimationFrame(raf)
+
   const container = useRef(null)
   const { scrollYProgress } = useScroll({
     target: container,

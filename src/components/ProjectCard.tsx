17be3519619e3ed3928dc36fc1b6c0907752a3a8
Ankit Singh <ankit.singh.zm@gmail.com>
2024-01-03 23:54:22 +0530

    made responsive + added link icons  ✅

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,7 +1,10 @@
+import { ImGithub } from "react-icons/im";
+import { BiLinkExternal } from "react-icons/bi";
+
 interface ItemProps {
-    key: string;
     name: string;
     projectImage: string;
+    links: { github: string, website: string };
     description: string;
     tags: Array<string>
 }
@@ -9,24 +12,28 @@ interface ItemProps {
 export function ProjectCard(props: ItemProps) {
 
     return (
-        <div
-            key={props.key}
-            className="rounded-2xl w-80 md:w-96 lg:w-128 h-128 sm:h-min p-1"
-        >
-            <img className="w-full aspect-video rounded-t-2xl" src={`projects/${props.projectImage}`} alt="Sunset in the mountains" loading="lazy" />
-            <div className="px-6 py-4 bg-opacity-20 bg-white backdrop-blur-3xl rounded-b-2xl text-black text-2xl">
-                <div className="font-bold mb-2">{props.name}</div>
-                <p className="text-gray-700 text-xl">
+        <div className="rounded-2xl w-80 md:w-96 lg:w-128 h-128 sm:h-min p-2 bg-green-600 bg-opacity-60">
+            <img className="w-full aspect-video rounded-2xl" src={`projects/${props.projectImage}`} alt="project image" loading="lazy" />
+            <div className="px-6 py-4 text-black text-xl md:text-2xl">
+                <div className="font-bold mb-2 flex justify-between">
+                    <span>{props.name}</span>
+                    <span className="flex gap-4 opacity-70">
+                        <a href={props.links.github} target="_blank">
+                            <ImGithub />
+                        </a>
+                        <a href={props.links.website} target="_blank">
+                            <BiLinkExternal />
+                        </a>
+                    </span>
+                </div>
+                <p className="text-gray-700 text-base md:text-xl">
                     {props.description}
                 </p>
                 <div className="pt-4 pb-2">
                     {props.tags.map((e: string) => (
-                        <span key={e} className="inline-block bg-gray-400 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{e}</span>
+                        <span key={e} className="inline-block bg-green-600 bg-opacity-30 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{e}</span>
                     ))}
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">tech</span>
-                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">web3</span>
                 </div>
-
             </div>
         </div>
     )
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -10,13 +10,13 @@ function Projects() {
 
       <div className="columns" id="columns">
         <div className="column column-reverse">
-          {projects.slice(0, totalProjects / 2).map(project => (
-            <ProjectCard name={project.name} projectImage={project.image} description={project.description} tags={["frontend", "express"]} key={project.name} />
+          {projects.slice(0, totalProjects / 2).map((project, index) => (
+            <ProjectCard key={index} name={project.name} projectImage={project.image} description={project.description} tags={project.tag} links={project.links} />
           ))}
         </div>
         <div className="column">
-          {projects.slice(totalProjects / 2, totalProjects).map(project => (
-            <ProjectCard name={project.name} projectImage={project.image} description={project.description} tags={["frontend", "express"]} key={project.name} />
+          {projects.slice(totalProjects / 2, totalProjects).map((project, index) => (
+            <ProjectCard key={index} name={project.name} projectImage={project.image} description={project.description} tags={project.tag} links={project.links} />
           ))}
         </div>
       </div>

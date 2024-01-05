89c34e2ecb402f42e46aaf96401e7b38afee4f5e
Ankit Singh <ankit.singh.zm@gmail.com>
2024-01-05 23:32:22 +0530

    UI edge case fixed (for odd number of project)

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -13,6 +13,17 @@ function Projects() {
           {projects.slice(0, totalProjects / 2).map((project, index) => (
             <ProjectCard key={index} name={project.name} projectImage={project.image} description={project.description} tags={project.tag} links={project.links} />
           ))}
+
+          {/* edge case(ui issue) - if number of projects are odd */}
+          {
+            projects.length % 2 && window.innerWidth > 700 ?
+              <div className="grid place-items-center rounded-2xl w-80 md:w-96 lg:w-128 h-128 p-2 isolate bg-white/20 shadow-xl ring-1 ring-black/5 text-2xl lg:text-4xl font-sans font-bold">
+                That's all for now !! <br /><br />
+                I might be building more, <br />
+                check out later : )
+              </div> :
+              <></>
+          }
         </div>
         <div className="column">
           {projects.slice(totalProjects / 2, totalProjects).map((project, index) => (

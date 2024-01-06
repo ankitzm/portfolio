b48689ac97952a7873ecc4250d36f7d956563bc1
Ankit Singh <ankit.singh.zm@gmail.com>
2024-01-06 16:28:02 +0530

    fixed window alignment

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,7 +5,7 @@ import { useRef } from "react";
 function Home() {
   const ref = useRef(null)
   const { scrollYProgress } = useScroll({ target: ref, offset: ["start start", "end end"] })
-  const scale = useTransform(scrollYProgress, [0, 0.4, 1], [1, 2, 10])
+  const scale = useTransform(scrollYProgress, [0, 0.2, 1], [1, 5, 20])
   const imageX = useTransform(scrollYProgress, [0, 1], [50, 0])
   const imageXSmallScreen = useTransform(scrollYProgress, [0, 1], [10, 0])
   const imageXCalc = useMotionTemplate`max(0px, calc(${window.innerWidth > 786 ? imageX : imageXSmallScreen}% + 3*calc(${imageX}vw/7)))`
@@ -33,13 +33,14 @@ function Home() {
           style={{ scale }}
           className="hero-bg sticky left-0 top-0 grid place-items-center h-screen w-full origin-[50%_80%] md:origin-[80%_40%] p-0 sm:p-10"
         >
-          <div className="flex flex-col md:flex-row md:h-screen w-full justify-center rounded-3xl z-10">
-            <div className="grid place-items-center h-[30vh] md:h-full bg-slate-400 w-full md:w-1/2 md:rounded-l-3xl font-extrabold text-2xl sm:text-4xl md:text-6xl text-slate-800">
-              Let's peek through <br />
+          <div className="flex flex-col md:flex-row md:h-screen w-full justify-center rounded-3xl z-10 bg-transparent">
+            <div className="grid place-items-center h-[30vh] md:h-full bg-slate-400 w-full md:w-1/2 rounded-t-2xl md:rounded-t-none md:rounded-l-3xl font-extrabold text-2xl md:text-4xl lg:text-6xl text-slate-800 spacing">
+              Peek through <br />
+              this 'window' <br />
               to see what <br />
               I have done so far !!
             </div>
-            <div className="window-mask grid place-items-center h-full bg-white py-16 w-full md:rounded-r-3xl md:w-1/2 min-w-[300px]">
+            <div className="window-mask grid place-items-center h-full bg-white py-16 w-full rounded-b-xl md:rounded-b-none md:rounded-r-3xl md:w-1/2 min-w-[300px]">
               {/* window */}
               <div className="my-auto w-[200px] sm:w-[300px] aspect-[5/8] rounded-full bg-slate-400">
               </div>

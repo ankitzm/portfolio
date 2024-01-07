b212c925d16e7e03c9b7387c85d91ec5461a0b5a
Ankit Singh <ankit.singh.zm@gmail.com>
2024-01-07 23:26:45 +0530

    removed unused import

--- src/components/motion-components/pageTransition.tsx
+++ src/components/motion-components/pageTransition.tsx
@@ -1,6 +1,5 @@
 import React from "react"
-import { easeIn, easeOut, motion } from "framer-motion"
-import { easeInOut } from "framer-motion/dom"
+import { motion, easeIn, easeOut } from "framer-motion"
 // import React from "react"
 // import { JSX } from "react/jsx-runtime"
 
@@ -12,14 +11,14 @@ const transition = (TheComponent: React.FunctionComponent) => {
                 initial={{ scaleY: 0 }}
                 animate={{ scaleY: 0 }}
                 exit={{ scaleY: 1, scaleX: 1 }}
-                transition={{ duration: 1, ease: "easeIn" }}
+                transition={{ duration: 1, ease: easeIn }}
             />
             <motion.div
                 className="fixed top-0 left-0 w-full h-screen bg-pastelOrange origin-top z-50"
                 initial={{ scaleY: 1 }}
                 animate={{ scaleY: 0 }}
                 exit={{ scaleY: 0 }}
-                transition={{ duration: 1, ease: "easeOut" }}
+                transition={{ duration: 1, ease: easeOut }}
             />
             <TheComponent />
         </>

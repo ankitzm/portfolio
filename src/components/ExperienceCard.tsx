749ab38a43c44d360791134dff8ce9995ddc1ff6
Ankit Singh <ankit.singh.zm@gmail.com>
2024-01-10 23:28:19 +0530

    card responsiveness fixed

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -2,8 +2,6 @@ import React, { ReactNode } from 'react';
 import Markdown from 'react-markdown';
 import { useTransform, motion, MotionValue } from 'framer-motion';
 
-import tailwindColors from 'tailwindcss/colors';
-
 interface MyComponentProps {
     index: number;
     color: string;
@@ -23,11 +21,7 @@ interface LinkComponentProps {
 
 const ExperienceCard: React.FC<MyComponentProps> =
     ({ index, color, company, role, date, description, skills, progress, range, targetScale }) => {
-        // Tailwind Color List
-        // console.log(Object.keys(tailwindColors));
-
-        const backgroundColor = Object.keys(tailwindColors).includes(color.toLocaleLowerCase()) ? `bg-${color}-400` : 'bg-slate-400'
-
+    
         const scale = useTransform(progress, range, [1, targetScale])
 
         // custom anchor tag
@@ -46,7 +40,7 @@ const ExperienceCard: React.FC<MyComponentProps> =
                     scale
                 }}
             >
-                <div className={`flex flex-col justify-center relative p-4 sm:p-6 lg:p-8 rounded-2xl w-[95vh] sm:min-w-[600px] md:w-[750px] lg:w-[1000px] h-screen sm:min-h-[600px] md:h-[550px] text-xl border-8 border-black border-opacity-5 ${backgroundColor} bg-opacity-[95]`} >
+                <div className={`flex flex-col justify-center relative p-4 sm:p-6 lg:p-8 rounded-2xl w-[95vh] sm:min-w-[600px] md:w-[750px] lg:w-[1000px] h-screen sm:min-h-[600px] md:h-[550px] text-xl border-8 border-black border-opacity-5 bg-${color}-300`} >
                     <div className="gap-2 sm:gap-6">
                         <h3 className="text-xl sm:text-3xl md:text-4xl font-bold text-gray-900">
                             {company}
@@ -57,9 +51,9 @@ const ExperienceCard: React.FC<MyComponentProps> =
                     </div>
 
                     <ul className="list-disc text-base sm:text-xl text-gray-600 selection:mt-6 sm:mt-8">
-                        {description.map((item: string | null | undefined) => {
+                        {description.map((item, index) => {
                             return (
-                                <li className='m-4'>
+                                <li className='m-2 sm:m-4' key={index}>
                                     <Markdown components={{
                                         a(props) {
                                             return <LinkComponent text={props.children} {...props} />
@@ -70,18 +64,18 @@ const ExperienceCard: React.FC<MyComponentProps> =
                         })}
                     </ul>
 
-                    <div className="mt-10 gap-4 sm:gap-6">
+                    <div className="mt-4 sm:mt-10 gap-4 sm:gap-6">
                         <div className="font-bold text-gray-600">Worked With/On :</div>
                         <div>
-                            {skills.map((tab: string) => {
+                            {skills.map((tab, index) => {
                                 return (
-                                    <p className='bg-white bg-opacity-30 backdrop-blur-3xl m-2 inline-block p-2 rounded-lg text-base sm:text-xl'>{tab}</p>
+                                    <p key={index} className='bg-white bg-opacity-30 backdrop-blur-3xl m-1 sm:m-2 inline-block p-2 rounded-lg text-sm sm:text-xl'>{tab}</p>
                                 )
                             })}
                         </div>
                     </div>
 
-                    <div className="absolute top-0 right-0 bg-white bg-opacity-30 backdrop-blur-xl p-4 sm-p-6 text-gray-700 italic rounded-tr-xl rounded-bl-xl">
+                    <div className="absolute top-6 sm:top-0 right-0 text-sm sm:text-xl bg-white bg-opacity-30 backdrop-blur-xl p-2 sm:p-6 text-gray-700 italic rounded-l-xl sm:rounded-tr-xl sm:rounded-bl-xl">
                         {date}
                     </div>
                 </div>

bfcff693bc2a6f107482b6fc69e432d2278b5fe9
Ankit Singh <ankit.singh.zm@gmail.com>
2024-01-11 19:09:54 +0530

    improved performance

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,22 +27,22 @@ function Navbar() {
     const currentPath = useLocation().pathname
     // console.log(currentPath);
 
-    const activeClass = 'bg-opacity-95'
+    const activeClass = 'bg-opacity-100'
 
     return (
-        <ul className="z-50 fixed flex md:flex-col font-medium">
+        <div className="z-50 fixed flex md:flex-col font-semibold text-base sm:text-lg lg:text-xl">
             {
                 navButtons.map((item, index) =>
                     <Link key={index} to={item.page.toLowerCase()}>
-                        <li key={index} className={`flex items-center gap-2 rounded-md w-fit text-base sm:text-lg py-1 px-3 sm:py-2 sm:px-4 m-2 ${currentPath == item.page ? activeClass : "bg-opacity-75"} bg-slate-200 border-2 border-dotted border-neutral-800 backdrop-blur-xl`}>
+                        <span key={index} className={`flex items-center gap-2 rounded-md w-fit py-1 px-3 sm:py-2 sm:px-4 m-2 ${currentPath == item.page ? activeClass : "bg-opacity-75"} bg-slate-200 border-2 border-dotted border-neutral-800 backdrop-blur-xl`}>
                             {<item.icon />} {item.name.toLowerCase()}
-                        </li>
+                        </span>
                     </Link>
                 )
             }
 
             
-        </ul>
+        </div>
     )
 }
 

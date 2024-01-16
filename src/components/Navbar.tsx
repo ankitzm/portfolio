c4526595fd5ca1b723b7e0eb52ac577bb6bc09db
Ankit Singh <ankit.singh.zm@gmail.com>
2024-01-16 15:21:14 +0530

    nav-link fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -30,11 +30,11 @@ function Navbar() {
     const activeClass = 'bg-opacity-100'
 
     return (
-        <div className="z-50 fixed flex flex-wrap md:flex-col font-medium sm:font-semibold text-sm sm:text-lg lg:text-xl w-full pt-4 pl-1 sm:pt-4 sm:pl-8">
+        <div className="z-50 fixed flex flex-wrap md:flex-col font-medium sm:font-semibold text-sm sm:text-lg lg:text-xl pt-4 pl-1 sm:pt-4 sm:pl-8">
             {
                 navButtons.map((item, index) =>
-                    <Link key={index} to={item.page.toLowerCase()}>
-                        <span key={index} className={`flex items-center gap-2 rounded-md w-fit py-1 px-3 sm:py-2 sm:px-4 m-1 sm:m-2 ${currentPath == item.page ? activeClass : "bg-opacity-75"} bg-slate-200 border-2 border-dotted border-neutral-800 backdrop-blur-xl`}>
+                    <Link key={index} to={item.page.toLowerCase()} className="w-fit">
+                        <span key={index} className={`flex items-center gap-2 w-fit rounded-md py-1 px-3 sm:py-2 sm:px-4 m-1 sm:m-2 ${currentPath == item.page ? activeClass : "bg-opacity-75"} bg-slate-200 border-2 border-dotted border-neutral-800 backdrop-blur-xl`}>
                             {<item.icon />} {item.name.toLowerCase()}
                         </span>
                     </Link>

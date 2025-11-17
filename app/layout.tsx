31fef7a9a3c3a8b93d0439317ccc8fc6c960dcea
Ankit Singh <66105983+ankitzm@users.noreply.github.com>
2025-11-17 20:33:35 +0530

    Add technical documentation for portfolio website, detailing project overview, tech stack, architecture, components, and development guidelines; update Home component text and layout for improved presentation.

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background-base h-screen overflow-hidden`}
       >
         <div className="h-screen p-4 pb-6 md:p-10 m-0">
-          <div className="bg-background h-full rounded-2xl overflow-y-auto">
+          <div className="bg-background h-full rounded-2xl overflow-y-auto flex flex-col items-center w-full justify-center">
           {children}
           </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   const timeoutsRef = useRef<number[]>([]);
 
   return (
-    <div className="font-sans items-center">
-      difhue
+    <div className="font-sans border-2  border-e-indigo-100">
+      Hi i am ankit singh
     </div>
   );
 }

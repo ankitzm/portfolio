0d37d2bbe6ce7bf25bf54420be17243d1e394e7e
Ankit Singh <66105983+ankitzm@users.noreply.github.com>
2025-11-20 17:58:59 +0530

    Add DelicateAsciiDots component for animated background effects; update Home component to include the new visual element and enhance layout with improved styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 import { useEffect, useRef, useState } from "react";
+import DelicateAsciiDots from "@/app/components/ui/delicate-ascii-dots";
 
 export default function Home() {
   const [progress, setProgress] = useState(0);
   const timeoutsRef = useRef<number[]>([]);
 
   return (
-    <div className="font-sans border-2  border-e-indigo-100">
-      Hi i am ankit singh
+    <div className="relative w-full h-screen font-sans overflow-hidden">
+      <DelicateAsciiDots />
+      <div className="relative z-10 flex items-center justify-center h-full font-san">
+        <h1 className="text-4xl font-bold">Hi !!</h1>
+      </div>
     </div>
   );
 }

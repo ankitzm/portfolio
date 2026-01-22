77b4f5b1ada60d71cd1964ea1a20e971626667b5
Ankit Singh <66105983+ankitzm@users.noreply.github.com>
2026-01-22 13:32:21 +0530

    Revert "feat:added snowflakes"
    This reverts commit 6cfc99f3719b49f0bad55587dbc6cfc96b12fcb8.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Poppins, Righteous } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import ClientLayout from "./components/ClientLayout";
+import NavBar from "./components/NavBar";
+import PageTransition from "./components/PageTransition";
+import { TransitionProvider } from "./components/TransitionContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

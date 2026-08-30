055aaadf5a0deba8adcf2faa6428e057b5eb9ac2
Ankit Singh <66105983+ankitzm@users.noreply.github.com>
2026-08-30 14:43:17 +0530

    chore: setup nextjs+tailwind+prettier

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-2xl font-semibold">Portfolio</h1>
+    </main>
+  );
+}
